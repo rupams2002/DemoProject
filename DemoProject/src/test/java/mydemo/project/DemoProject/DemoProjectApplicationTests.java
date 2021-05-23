75f55cdad8dac111cba3061aa70158cdbdfe9e1c
Mohammed Hasan <rupams2002@yahoo.com>
2021-05-22 23:00:07 -0400

    First push

---
+++ DemoProject/src/test/java/mydemo/project/DemoProject/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mydemo.project.DemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
